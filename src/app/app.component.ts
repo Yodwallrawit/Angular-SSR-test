96dd5051faee2f553e76da471efb31beef5b19a9
Yodwallrawit <yodwallrawit@wisdomvast.com>
2021-11-29 09:15:36 +0700

    3nd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SeoAndLogin';
+  href: string;
 
-  
+  constructor(private route: ActivatedRoute,private router: Router,location:Location) { 
+
+  }
+
+  ngOnInit(): void {
+    this.href = this.router.url;
+    console.log(this.router.url);
+  }
 }
